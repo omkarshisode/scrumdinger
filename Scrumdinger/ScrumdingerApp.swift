00b3331e5a879982b1570c4e9ac2a9f085cfc7bb
omkarshisode123 <omkar.shisode@dista.ai>
2025-05-16 10:17:50 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Omkar Shisode on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
