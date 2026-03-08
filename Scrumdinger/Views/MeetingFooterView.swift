fce2ef6dc86e9b196ebb8b790b3a1d1a207fd892
Omkar Shisode <76614558+omkarshisode@users.noreply.github.com>
2026-03-08 17:01:38 +0530

    - Managing the view life cycle

---
+++ Scrumdinger/Views/MeetingFooterView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingFooterView.swift
+//  Scrumdinger
+//
+//  Created by Omkar Shisode on 08/03/26.
+//
+
+import SwiftUI
+
+struct MeetingFooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingFooterView()
+}
